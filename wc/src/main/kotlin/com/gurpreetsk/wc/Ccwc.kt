e6aacdf200c594214a2f3e644c96d33121680498
Gurpreet Singh <gurpreetsk@hotmail.com>
2024-01-08 21:43:33 -0800

    Read bytes in blocks to calculate file size.

--- wc/src/main/kotlin/com/gurpreetsk/wc/Ccwc.kt
+++ wc/src/main/kotlin/com/gurpreetsk/wc/Ccwc.kt
@@ -5,7 +5,8 @@ import java.io.File
 object Ccwc {
 
     fun run(arg: SupportedArgs, path: File): Pair<Long, String> {
-        val size = path.readBytes().size.toLong()
+        var size = 0L
+        path.forEachBlock { _, bytesRead: Int -> size += bytesRead }
 
         return Pair(size, "test.txt")
     }
