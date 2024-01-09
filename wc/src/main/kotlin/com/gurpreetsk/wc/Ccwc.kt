4be85c0cfd7cde95f09c5cf64beac080af6834ac
Gurpreet Singh <gurpreetsk@hotmail.com>
2024-01-08 21:24:47 -0800

    Naive approach - Read bytes to calculate file size.

--- wc/src/main/kotlin/com/gurpreetsk/wc/Ccwc.kt
+++ wc/src/main/kotlin/com/gurpreetsk/wc/Ccwc.kt
@@ -5,7 +5,7 @@ import java.io.File
 object Ccwc {
 
     fun run(arg: SupportedArgs, path: File): Pair<Long, String> {
-        val size = path.length()
+        val size = path.readBytes().size.toLong()
 
         return Pair(size, "test.txt")
     }
