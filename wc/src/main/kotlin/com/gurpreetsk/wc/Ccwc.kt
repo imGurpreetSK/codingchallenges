e0fdeedb9a7059f4f14222a6aa70d13212b4ac21
Gurpreet Singh <gurpreetsk@hotmail.com>
2024-01-09 18:45:05 -0800

    Extract logic to get file byte count to a function.

--- wc/src/main/kotlin/com/gurpreetsk/wc/Ccwc.kt
+++ wc/src/main/kotlin/com/gurpreetsk/wc/Ccwc.kt
@@ -5,10 +5,14 @@ import java.io.File
 object Ccwc {
 
     fun run(arg: SupportedArgs, path: File): Pair<Long, String> {
+        return getByteCountFor(path)
+    }
+
+    private fun getByteCountFor(path: File): Pair<Long, String> {
         var size = 0L
         path.forEachBlock { _, bytesRead: Int -> size += bytesRead }
 
-        return Pair(size, "test.txt")
+        return Pair(size, path.name)
     }
 
     enum class SupportedArgs(val arg: String) {
