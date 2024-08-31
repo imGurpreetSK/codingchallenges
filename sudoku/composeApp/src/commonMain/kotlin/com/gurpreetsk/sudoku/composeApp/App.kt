0da72247915915529213076da06875176ec919dc
Gurpreet Singh <gurpreetsk@hotmail.com>
2024-08-31 13:52:35 -0700

    Make grid populate dynamically.

--- sudoku/composeApp/src/commonMain/kotlin/com/gurpreetsk/sudoku/composeApp/App.kt
+++ sudoku/composeApp/src/commonMain/kotlin/com/gurpreetsk/sudoku/composeApp/App.kt
@@ -4,7 +4,7 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
@@ -15,8 +15,87 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Preview
 fun App() {
     MaterialTheme {
+        val data by remember {
+            mutableStateOf(
+                GridData(
+                    arrayOf(
+                        arrayOf(
+                            Cell(
+                                arrayOf(
+                                    arrayOf(1u, 1u, 1u),
+                                    arrayOf(1u, 1u, 1u),
+                                    arrayOf(1u, 1u, 1u),
+                                )
+                            ),
+                            Cell(
+                                arrayOf(
+                                    arrayOf(2u, 2u, 2u),
+                                    arrayOf(2u, 2u, 2u),
+                                    arrayOf(2u, 2u, 2u),
+                                )
+                            ),
+                            Cell(
+                                arrayOf(
+                                    arrayOf(3u, 3u, 3u),
+                                    arrayOf(3u, 3u, 3u),
+                                    arrayOf(3u, 3u, 3u),
+                                )
+                            )
+                        ),
+                        arrayOf(
+                            Cell(
+                                arrayOf(
+                                    arrayOf(4u, 4u, 4u),
+                                    arrayOf(4u, 4u, 4u),
+                                    arrayOf(4u, 4u, 4u),
+                                )
+                            ),
+                            Cell(
+                                arrayOf(
+                                    arrayOf(5u, 5u, 5u),
+                                    arrayOf(5u, 5u, 5u),
+                                    arrayOf(5u, 5u, 5u),
+                                )
+                            ),
+                            Cell(
+                                arrayOf(
+                                    arrayOf(6u, 6u, 6u),
+                                    arrayOf(6u, 6u, 6u),
+                                    arrayOf(6u, 6u, 6u),
+                                )
+                            )
+                        ),
+                        arrayOf(
+                            Cell(
+                                arrayOf(
+                                    arrayOf(7u, 7u, 7u),
+                                    arrayOf(7u, 7u, 7u),
+                                    arrayOf(7u, 7u, 7u),
+                                )
+                            ),
+                            Cell(
+                                arrayOf(
+                                    arrayOf(8u, 8u, 8u),
+                                    arrayOf(8u, 8u, 8u),
+                                    arrayOf(8u, 8u, 8u),
+                                )
+                            ),
+                            Cell(
+                                arrayOf(
+                                    arrayOf(9u, 9u, 9u),
+                                    arrayOf(9u, 9u, 9u),
+                                    arrayOf(9u, 9u, 9u),
+                                )
+                            )
+                        ),
+                    )
+                )
+            )
+        }
+
         Grid(
-            Modifier
+            data,
+            modifier = Modifier
                 .aspectRatio(1f)
                 .fillMaxSize()
         )
@@ -24,7 +103,10 @@ fun App() {
 }
 
 @Composable
-private fun Grid(modifier: Modifier = Modifier) {
+private fun Grid(
+    data: GridData,
+    modifier: Modifier = Modifier
+) {
     Column(
         modifier = modifier
             .border(0.5.dp, Color.Black)
@@ -33,7 +115,10 @@ private fun Grid(modifier: Modifier = Modifier) {
         for (i in 0 until 3) {
             Row(modifier = Modifier.weight(0.33f)) {
                 for (j in 0 until 3) {
-                    UnitBox(Modifier.weight(0.33f))
+                    UnitBox(
+                        data.items[i][j],
+                        Modifier.weight(0.33f)
+                    )
                 }
             }
         }
@@ -41,102 +126,32 @@ private fun Grid(modifier: Modifier = Modifier) {
 }
 
 @Composable
-private fun UnitBox(modifier: Modifier = Modifier) {
+private fun UnitBox(
+    data: Cell,
+    modifier: Modifier = Modifier
+) {
     Column(
         modifier = modifier
             .border(0.36.dp, Color.Black)
     ) {
-        Row(
-            modifier = Modifier
-                .weight(0.33f)
-                .wrapContentSize()
-        ) {
-            Text(
-                "A",
-                textAlign = TextAlign.Center,
-                maxLines = 1,
+        for (i in 0..2) {
+            Row(
                 modifier = Modifier
-                    .aspectRatio(1f)
-                    .border(0.3.dp, Color.Gray)
-            )
-            Text(
-                "B",
-                textAlign = TextAlign.Center,
-                maxLines = 1,
-                modifier = Modifier
-                    .aspectRatio(1f)
-                    .border(0.3.dp, Color.Gray)
-            )
-            Text(
-                "C",
-                textAlign = TextAlign.Center,
-                maxLines = 1,
-                modifier = Modifier
-                    .aspectRatio(1f)
-                    .border(0.3.dp, Color.Gray)
-            )
-        }
+                    .weight(0.33f)
+                    .wrapContentSize()
+            ) {
+                for (j in 0..2) {
+                    Text(
+                        data.items[i][j]?.toString() ?: "",
+                        textAlign = TextAlign.Center,
+                        maxLines = 1,
+                        modifier = Modifier
+                            .aspectRatio(1f)
+                            .border(0.3.dp, Color.Gray)
+                    )
+                }
+            }
 
-        Row(
-            modifier = Modifier
-                .weight(0.33f)
-                .wrapContentSize()
-        ) {
-            Text(
-                "D",
-                textAlign = TextAlign.Center,
-                maxLines = 1,
-                modifier = Modifier
-                    .aspectRatio(1f)
-                    .border(0.3.dp, Color.Gray)
-            )
-            Text(
-                "E",
-                textAlign = TextAlign.Center,
-                maxLines = 1,
-                modifier = Modifier
-                    .aspectRatio(1f)
-                    .border(0.3.dp, Color.Gray)
-            )
-            Text(
-                "F",
-                textAlign = TextAlign.Center,
-                maxLines = 1,
-                modifier = Modifier
-                    .aspectRatio(1f)
-                    .border(0.3.dp, Color.Gray)
-            )
-        }
-
-        Row(
-            modifier = Modifier
-                .weight(0.33f)
-                .wrapContentSize()
-        ) {
-            Text(
-                "G",
-                textAlign = TextAlign.Center,
-                maxLines = 1,
-                modifier = Modifier
-                    .aspectRatio(1f)
-                    .border(0.3.dp, Color.Gray)
-            )
-            Text(
-                "H",
-                textAlign = TextAlign.Center,
-                maxLines = 1,
-                modifier = Modifier
-                    .aspectRatio(1f)
-                    .border(0.3.dp, Color.Gray)
-            )
-            Text(
-                "I",
-                textAlign = TextAlign.Center,
-                maxLines = 1,
-                modifier = Modifier
-                    .aspectRatio(1f)
-                    .border(0.3.dp, Color.Gray)
-            )
         }
     }
 }
---
+++ sudoku/composeApp/src/commonMain/kotlin/com/gurpreetsk/sudoku/composeApp/GridData.kt
@@ -0,0 +1,37 @@
+package com.gurpreetsk.sudoku.composeApp
+
+data class GridData(
+    val items: Array<Array<Cell>>
+) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as GridData
+
+        return items.contentDeepEquals(other.items)
+    }
+
+    override fun hashCode(): Int {
+        return items.contentDeepHashCode()
+    }
+}
+
+data class Cell(
+    val items: Array<Array<UInt?>>
+) {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Cell
+
+        return items.contentDeepEquals(other.items)
+    }
+
+    override fun hashCode(): Int {
+        return items.contentDeepHashCode()
+    }
+}
