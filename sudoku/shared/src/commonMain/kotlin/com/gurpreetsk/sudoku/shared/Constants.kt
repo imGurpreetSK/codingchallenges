09e0ae46815c1cf150684df1eb62313de338dea9
Gurpreet Singh <gurpreetsk@hotmail.com>
2024-09-01 00:50:08 -0700

    Delete unwanted auto-generated classes.

--- sudoku/shared/src/commonMain/kotlin/com/gurpreetsk/sudoku/shared/Constants.kt
+++
@@ -1,3 +0,0 @@
-package com.gurpreetsk.sudoku.shared
-
-const val SERVER_PORT = 8080
--- sudoku/shared/src/commonMain/kotlin/com/gurpreetsk/sudoku/shared/Platform.kt
+++
@@ -1,9 +0,0 @@
-package com.gurpreetsk.sudoku.shared
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
-
-
